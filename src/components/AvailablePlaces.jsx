d4af92b12205539b7b45034a7979caa97c9389b2
Ghazaleh Ansari <ghazaaleh.ansari@gmail.com>
2024-04-15 12:49:43 +0200

    Initial commit

---
+++ src/components/AvailablePlaces.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Places from "./Places.jsx";
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
