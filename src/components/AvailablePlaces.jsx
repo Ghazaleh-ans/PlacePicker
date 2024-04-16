51553c6be40044faa58610be792bc870d8287680
Ghazaleh Ansari <ghazaaleh.ansari@gmail.com>
2024-04-16 20:40:02 +0200

    sending data with POST request

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -10,19 +11,13 @@ export default function AvailablePlaces({ onSelectPlace }) {
 
   useEffect(() => {
     setIsFetching(true);
-
     async function fetchPlaces() {
       try {
-        const response = await fetch("http://localhost:3000/places");
-        const resData = await response.json();
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch places");
-        }
-
+        const places = await fetchAvailablePlaces();
+        
         navigator.geolocation.getCurrentPosition((position) => {
           const sortedPlaces = sortPlacesByDistance(
-            resData.places,
+            places,
             position.coords.latitude,
             position.coords.longitude
           );
